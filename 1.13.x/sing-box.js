dcbf6cb96fecb383fec8fb1483344a413765e2f7
Mojit0 <yan199219dang@gmail.com>
2026-02-07 09:54:22 +0800

    Update sing-box.js

--- 1.13.x/sing-box.js
+++ 1.13.x/sing-box.js
@@ -1,4 +1,4 @@
-const { type, name } = $arguments
+const { type = '1', name = 'General' } = $arguments
 const compatible_outbound = {
   tag: 'COMPATIBLE',
   type: 'direct',
@@ -16,30 +16,30 @@ let proxies = await produceArtifact({
 config.outbounds.push(...proxies)
 
 config.outbounds.map(i => {
-  if (['🍀𝕌𝕝𝕥𝕚𝕞𝕒𝕥𝕖'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies))
-  }
-  if (['🤖𝔸𝕚𝔸𝕘𝕖𝕟𝕥'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^(?!.*(?:✰✰|★★)).*(?:美国|US|🇺🇸|自建).*$/i))
-  }
-  if (['🎓𝔻𝕦𝕠𝕝𝕚𝕟𝕘𝕠', '🎵𝕊𝕡𝕠𝕥𝕚𝕗𝕪'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /(?:新加坡|SG|🇸🇬).*$/i))
-  }
-  if (['🎨𝕐𝕠𝕦𝕋𝕦𝕓𝕖'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^((?!✰✰|★★|微斯).)*$/i))
+  if (['🎬𝕄𝕖𝕕𝕚𝕒'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:✰✰|★★|美国|US|🇺🇸|日本|JP|🇯🇵|微斯)).*(?:新加坡|SG|🇸🇬|台湾|TW|🇹🇼|🇨🇳|自建).*$/i))
   }
   if (['🎧𝕋𝕚𝕜𝕥𝕠𝕜'].includes(i.tag)) {
     i.outbounds.push(...getTags(proxies, /^(?!.*(?:✰✰|★★|日本|JP|🇯🇵|微斯)).*(?:台湾|TW|🇹🇼|🇨🇳|自建).*$/i))
   }
   if (['🐳𝕋𝕨𝕚𝕥𝕥𝕖𝕣'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^(?!.*(?:微斯)).*(?:美国|US|🇺🇸新加坡|SG|🇸🇬|台湾|TW|🇹🇼|🇨🇳|自建).*$/i))
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:微斯)).*(?:美国|US|🇺🇸|新加坡|SG|🇸🇬|台湾|TW|🇹🇼|🇨🇳|日本|🇯🇵|自建).*$/i))
   }
-  if (['🎬𝕄𝕖𝕕𝕚𝕒'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^(?!.*(?:✰✰|★★|美国|US|🇺🇸|日本|JP|🇯🇵|微斯)).*(?:新加坡|SG|🇸🇬|台湾|TW|🇹🇼|🇨🇳|自建).*$/i))
+  if (['🤖𝔸𝕚𝔸𝕘𝕖𝕟𝕥'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^(?!.*(?:✰✰|★★)).*(?:美国|US|🇺🇸|自建).*$/i))
   }
-  if (['☁️ℙ𝕚𝕜ℙ𝕒𝕜'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies, /^(?!.*(?:✰✰|★★|美国|US|🇺🇸|日本|JP|🇯🇵|微斯)).*(?:新加坡|SG|🇸🇬|台湾|TW|🇹🇼|🇨🇳|自建).*$/i))
+  if (['🎨𝕐𝕠𝕦𝕋𝕦𝕓𝕖'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /^((?!✰✰|★★|微斯).)*$/i))
   }
+  if (['🍀𝕌𝕝𝕥𝕚𝕞𝕒𝕥𝕖'].includes(i.tag)) {
+    i.outbounds.push(...getTags(proxies, /(?:新加坡|SG|🇸🇬|香港|🇭🇰|台湾|🇹🇼|日本|🇯🇵|自建).*$/i))
+  }
+  // if (['☁️ℙ𝕚𝕜ℙ𝕒𝕜'].includes(i.tag)) {
+  //   i.outbounds.push(...getTags(proxies, /^(?!.*(?:✰✰|★★|美国|US|🇺🇸|日本|JP|🇯🇵|微斯)).*(?:新加坡|SG|🇸🇬|台湾|TW|🇹🇼|🇨🇳|自建).*$/i))
+  // }
+  // if (['🎓𝔻𝕦𝕠𝕝𝕚𝕟𝕘𝕠', '🎵𝕊𝕡𝕠𝕥𝕚𝕗𝕪'].includes(i.tag)) {
+  //   i.outbounds.push(...getTags(proxies, /(?:新加坡|SG|🇸🇬).*$/i))
+  // }
 })
 
 config.outbounds.forEach(outbound => {
